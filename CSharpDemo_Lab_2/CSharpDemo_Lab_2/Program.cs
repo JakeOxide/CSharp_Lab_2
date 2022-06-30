df343aa828205ed1a6d58f9ef654f993608e5029
JakeOxide <101085109+JakeOxide@users.noreply.github.com>
2022-06-30 10:34:49 +0300

    Added Classes (trial)
    Trial classes added.
    These classes will be modified, but also they might be completely changed.

---
+++ CSharpDemo_Lab_2/CSharpDemo_Lab_2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharpDemo_Lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Ast
+    {
+        private const string name = "CSharp Assistant";
+        private const int identifier = 1288435;
+
+        
+
+    }
+
+    class Operations
+    {
+        private string? name;
+        private int? ID;
+
+        public Operations()
+        {
+            
+        }
+    }
+}
